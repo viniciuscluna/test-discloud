7abcd113ba4edf1e39d14c5e825d51e00d9c3bb5
Vinicius Luna <viniciuscluna@gmail.com>
2026-02-26 18:26:53 -0300

    fix: update greeting message in main.go from "Hello, World!" to "Hello, Amogus!"

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,7 +10,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Hello, Amogus!")
 	})
 
 	log.Fatal(app.Listen(":8080"))
